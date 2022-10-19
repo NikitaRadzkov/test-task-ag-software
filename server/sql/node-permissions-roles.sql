20af8174b8a024f3e8a0fbd88e4c72dcbdfa293d
Mikita Radzkou <mikita.radzkou@boweryvaluation.com>
2022-10-19 18:28:20 +0300

    Add new sql queries

---
+++ server/sql/node-permissions-roles.sql
@@ -0,0 +1,32 @@
+-- Admin
+INSERT INTO RolePermissions(role_id, perm_id, createdAt, updatedAt) VALUES(1, 1, now(), now());
+INSERT INTO RolePermissions(role_id, perm_id, createdAt, updatedAt) VALUES(1, 2, now(), now());
+INSERT INTO RolePermissions(role_id, perm_id, createdAt, updatedAt) VALUES(1, 3, now(), now());
+INSERT INTO RolePermissions(role_id, perm_id, createdAt, updatedAt) VALUES(1, 4, now(), now());
+INSERT INTO RolePermissions(role_id, perm_id, createdAt, updatedAt) VALUES(1, 5, now(), now());
+INSERT INTO RolePermissions(role_id, perm_id, createdAt, updatedAt) VALUES(1, 6, now(), now());
+INSERT INTO RolePermissions(role_id, perm_id, createdAt, updatedAt) VALUES(1, 7, now(), now());
+INSERT INTO RolePermissions(role_id, perm_id, createdAt, updatedAt) VALUES(1, 8, now(), now());
+INSERT INTO RolePermissions(role_id, perm_id, createdAt, updatedAt) VALUES(1, 9, now(), now());
+INSERT INTO RolePermissions(role_id, perm_id, createdAt, updatedAt) VALUES(1, 10, now(), now());
+INSERT INTO RolePermissions(role_id, perm_id, createdAt, updatedAt) VALUES(1, 11, now(), now());
+INSERT INTO RolePermissions(role_id, perm_id, createdAt, updatedAt) VALUES(1, 12, now(), now());
+INSERT INTO RolePermissions(role_id, perm_id, createdAt, updatedAt) VALUES(1, 13, now(), now());
+INSERT INTO RolePermissions(role_id, perm_id, createdAt, updatedAt) VALUES(1, 14, now(), now());
+INSERT INTO RolePermissions(role_id, perm_id, createdAt, updatedAt) VALUES(1, 15, now(), now());
+INSERT INTO RolePermissions(role_id, perm_id, createdAt, updatedAt) VALUES(1, 16, now(), now());
+INSERT INTO RolePermissions(role_id, perm_id, createdAt, updatedAt) VALUES(1, 17, now(), now());
+INSERT INTO RolePermissions(role_id, perm_id, createdAt, updatedAt) VALUES(1, 18, now(), now());
+INSERT INTO RolePermissions(role_id, perm_id, createdAt, updatedAt) VALUES(1, 19, now(), now());
+INSERT INTO RolePermissions(role_id, perm_id, createdAt, updatedAt) VALUES(1, 20, now(), now());
+
+--Editor
+INSERT INTO RolePermissions(role_id, perm_id, createdAt, updatedAt) VALUES(2, 16, now(), now());
+INSERT INTO RolePermissions(role_id, perm_id, createdAt, updatedAt) VALUES(2, 17, now(), now());
+INSERT INTO RolePermissions(role_id, perm_id, createdAt, updatedAt) VALUES(2, 18, now(), now());
+INSERT INTO RolePermissions(role_id, perm_id, createdAt, updatedAt) VALUES(2, 19, now(), now());
+
+
+--User
+INSERT INTO RolePermissions(role_id, perm_id, createdAt, updatedAt) VALUES(3, 18, now(), now());
+INSERT INTO RolePermissions(role_id, perm_id, createdAt, updatedAt) VALUES(3, 19, now(), now());
