2885b624612068d8d8e877f7ed21f536d3727b12
Mikita Radzkou <mikita.radzkou@boweryvaluation.com>
2022-10-20 09:43:21 +0300

    Add PreviewTitle component

---
+++ client/src/components/PreviewTitle/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './styles.css';
+
+const PreviewTitle = () => (
+  <div className='preview-title'>
+    <h2>Inc. This Morning</h2>
+    <h1>
+      <span>“</span> Blog <span>”</span>
+    </h1>
+    <p>
+      awesome place to make oneself <br /> productive and entertained through
+      daily updates.
+    </p>
+  </div>
+);
+
+export default PreviewTitle;
